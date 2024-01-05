64a40599aa7d2052f7510f92ea53b721cdbb5233
DavidJoanes <david.kemdirim@yahoo.com>
2024-01-05 15:48:27 +0100

    further commit

--- main.dart.js
+++ main.dart.js
@@ -100173,17 +100173,17 @@ l=A.qw(m?B.e:B.V,1)
 k=A.aP0(20)
 p=A.p(p,f,f,f,a0,f,f)
 a1=A.oX(!1,f,!0,A.ay(f,A.aC(B.R,!0,f,!1,k,f,f,f,!1,f,A.p(o,f,f,f,a2,f,f),p,f),B.j,f,f,new A.aG(f,f,l,n,f,f,B.t),f,f,f,new A.Z(a1,10,a1,10),f,f,f),f,!0,f,f,f,f,f,f,f,f,f,new A.azX(g),f,f,f,f,f)
-n=a*0.04
 a3=a3.c.c.a
 p=$.h
 if(p==null)p=$.h=new A.z()
 p.C(a3.A$)
 a3=J.d(a3.D$,e)?B.au:B.L
-p=s*0.02
+p=a*0.02
+o=s*0.02
 m=g.w
-if(m===$){o=g.a.r
-o===$&&A.c()
-m=g.w=o}o=g.qU(a5,c,B.JK,"Dashboard",m,new A.azY(g))
+if(m===$){n=g.a.r
+n===$&&A.c()
+m=g.w=n}n=g.qU(a5,c,B.JK,"Dashboard",m,new A.azY(g))
 m=g.x
 if(m===$){l=g.a.w
 l===$&&A.c()
@@ -100204,7 +100204,7 @@ m=g.as
 if(m===$){h=g.a.Q
 h===$&&A.c()
 m=g.as=h}h=t.p
-return A.b2(A.ay(f,new A.a7(B.oT,A.ai(A.b([new A.r(f,q,f,f),a1,new A.r(f,s*0.03,f,f),B.bT,new A.a7(new A.Z(n,0,n,0),new A.r(f,s*0.7,A.hE(f,a3,A.hD(!0,A.hG(A.ai(A.b([new A.r(f,p,f,f),o,new A.r(f,p,f,f),l,new A.r(f,p,f,f),k,new A.r(f,p,f,f),j,new A.r(f,p,f,f),i,new A.r(f,p,f,f),g.qU(a5,c,B.ps,"Logs",m,new A.aA2(g)),new A.r(f,p,f,f)],h),B.o,f,B.n,B.h),f,B.J),B.R,!0),f),f),f),B.bT,new A.r(a*0.18,f,A.aC(f,!0,f,!1,new A.Gv(b,a*0.02,f),f,f,f,!1,f,f,f,A.e4(f,A.p("Logout",f,f,f,r,f,f),B.G,!1,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,new A.aA3(g,a5,c),f,f,f,f,f,f,!1,B.ar)),f),new A.r(f,q,f,f)],h),B.o,f,B.n,B.h),f),B.j,a4,f,f,f,s,f,f,f,f,a),f,40,B.R)},
+return A.b2(A.ay(f,new A.a7(B.oT,A.ai(A.b([new A.r(f,q,f,f),a1,new A.r(f,s*0.03,f,f),B.bT,new A.r(f,s*0.7,A.hE(f,a3,A.hD(!0,A.hG(new A.a7(new A.Z(p,0,p,0),A.ai(A.b([new A.r(f,o,f,f),n,new A.r(f,o,f,f),l,new A.r(f,o,f,f),k,new A.r(f,o,f,f),j,new A.r(f,o,f,f),i,new A.r(f,o,f,f),g.qU(a5,c,B.ps,"Logs",m,new A.aA2(g)),new A.r(f,o,f,f)],h),B.o,f,B.n,B.h),f),f,B.J),B.R,!0),f),f),B.bT,new A.r(a*0.18,f,A.aC(f,!0,f,!1,new A.Gv(b,p,f),f,f,f,!1,f,f,f,A.e4(f,A.p("Logout",f,f,f,r,f,f),B.G,!1,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,new A.aA3(g,a5,c),f,f,f,f,f,f,!1,B.ar)),f),new A.r(f,q,f,f)],h),B.o,f,B.n,B.h),f),B.j,a4,f,f,f,s,f,f,f,f,a),f,40,B.R)},
 qU(a,b,c,d,e,f){var s=null,r=b.b,q=b.a,p=A.cj(c,B.e,s,q*0.028),o=A.p(d,s,s,s,this.f,s,s),n=A.ct(10),m=e?B.e:B.V
 return new A.r(s,r*0.1,A.aC(s,!0,q*0.01,!1,p,r*0.025,s,new A.aA4(f),!1,new A.dH(n,new A.bT(m,1,B.a2,-1)),s,o,s),s)}}
 A.azX.prototype={
